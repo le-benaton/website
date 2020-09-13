b9cd58302149ef77f17848fc6d4b9c0545c65a75
榊原昌彦 <sakakibara@rdlabo.jp>
2020-09-13 19:45:17 +0900

    feat(): ビルド設定を追加

---
+++ scripts/build-data/index.ts
@@ -0,0 +1,41 @@
+import { mkdirSync, readFileSync, writeFileSync, rmdirSync } from 'fs';
+import { execSync } from 'child_process';
+
+import { news } from '../../src/data/news';
+import { redWine } from '../../src/data/red-wine';
+import { whiteWine } from '../../src/data/white-wine';
+import { champagneWine } from '../../src/data/champagne-wine';
+
+export interface IItem {
+  selector: string;
+  template: string;
+  data: Array<{ title?: string; ja?: string; price?: string; body?: string }>;
+}
+
+const targets = ['index.html'];
+const items: IItem[] = [news, redWine, whiteWine, champagneWine];
+
+const build = () => {
+  execSync(`rm -r -f ${process.cwd() + '/www'}`);
+  mkdirSync(process.cwd() + '/www');
+  execSync(`cp -r ${process.cwd() + '/src/template'}/* ${process.cwd() + '/www'}`);
+
+  for(const templateFile of targets) {
+    let template = readFileSync(process.cwd() + '/www/' + templateFile).toString('utf-8');
+    for(const item of items) {
+      let insertData = '';
+      for (const line of item.data) {
+        let template = item.template;
+        for (const [key, value] of Object.entries(line)) {
+          template = template.replace(new RegExp(`{{ ${key} }}`,"g"), value as string);
+        }
+        insertData += template;
+      }
+      const changeSelector = item.selector.replace(/></g, `>${insertData}<`);
+      template = template.replace(item.selector, changeSelector);
+    }
+    writeFileSync(process.cwd() + '/www/' + templateFile, template);
+  }
+}
+
+build();
