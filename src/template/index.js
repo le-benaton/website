d18ee36fc3e8d689e437f7b0245979d22c6b98bb
榊原昌彦 <sakakibara@rdlabo.jp>
2020-11-09 22:13:44 +0900

    format(): jsを追加

--- src/template/index.js
+++ src/template/index.js
@@ -6,7 +6,7 @@ let db;
  */
 const linkScroll = () => {
   const links = document.querySelectorAll('nav a[href]');
-  for(const link of links) {
+  for (const link of links) {
     if (!link.hash) {
       continue;
     }
@@ -29,7 +29,7 @@ const linkScroll = () => {
         return p * (2 - p);
       };
       const move = () => {
-        progress ++;
+        progress++;
         position = target * easeOut(progress / 20);
         window.scrollTo(0, position);
         if (position < target) {
@@ -37,9 +37,9 @@ const linkScroll = () => {
         }
       };
       requestAnimationFrame(move);
-    })
+    });
   }
-}
+};
 
 /**
  * モーダルを開くためのイベント
@@ -47,8 +47,8 @@ const linkScroll = () => {
  */
 const openModal = (selector) => {
   try {
-    gtag('event', 'select_content', { 'content_type': 'modal' + selector });
-  } catch (e) { }
+    gtag('event', 'select_content', { content_type: 'modal' + selector });
+  } catch (e) {}
 
   const innerHtml = document.querySelector(selector).innerHTML;
   const modal = document.querySelector('section.modal-wine-list');
@@ -56,14 +56,11 @@ const openModal = (selector) => {
   modalInner.innerHTML = innerHtml;
   modal.style.display = 'block';
 
-  modalInner.querySelector("form.wine-search-form").addEventListener('submit', (event) => {
+  modalInner.querySelector('form.wine-search-form').addEventListener('submit', (event) => {
     event.preventDefault();
-    searchWine(
-      event.target.querySelector('[type=search]').value,
-      event.target.querySelector('[type=hidden]').value,
-    );
-  })
-}
+    searchWine(event.target.querySelector('[type=search]').value, event.target.querySelector('[type=hidden]').value);
+  });
+};
 
 /**
  * モーダルを閉じるためのイベント
@@ -74,7 +71,7 @@ const closeModal = () => {
   modalInner.innerHTML = '';
   modalInner.scrollTop = 0;
   modal.style.display = 'none';
-}
+};
 
 /**
  *  クラス `.view_timer` がついていた時、
@@ -85,18 +82,15 @@ const viewTimerHandler = () => {
   const elements = document.querySelectorAll('.view_timer');
   elements.forEach((element) => {
     const start = element.getAttribute('data-start-date');
-    const end =  element.getAttribute('data-end-date');
+    const end = element.getAttribute('data-end-date');
     if (!start && !end) {
       return;
     }
-    if (
-      new Date().getTime() > new Date(start).getTime() &&
-      new Date().getTime() < new Date(end).getTime()
-    ) {
+    if (new Date().getTime() > new Date(start).getTime() && new Date().getTime() < new Date(end).getTime()) {
       element.style.display = 'list-item';
     }
   });
-}
+};
 
 /**
  * モバイルデバイスでハンバーガーメニューを
@@ -112,17 +106,21 @@ const toggleNav = (link) => {
     link.classList.add('active');
     nav.style.display = 'block';
   }
-}
+};
 
 /**
  * toggleNav() をイベントリスナーで確認
  */
 const mobileMenu = () => {
   const link = document.querySelector('#mobile-menu-button');
-  link.addEventListener('click', (e) => {
-    toggleNav(link);
-  }, { passive: true });
-}
+  link.addEventListener(
+    'click',
+    (e) => {
+      toggleNav(link);
+    },
+    { passive: true },
+  );
+};
 
 /**
  * `#menu-reserved` を `#copy-reserved` に
@@ -130,7 +128,7 @@ const mobileMenu = () => {
  */
 const copyReserved = () => {
   document.querySelector('#copy-reserved').innerHTML = document.querySelector('#menu-reserved').innerHTML;
-}
+};
 
 const getUserId = () => {
   let analyticsId = localStorage.getItem('analyticsId');
@@ -139,34 +137,38 @@ const getUserId = () => {
     localStorage.setItem('analyticsId', 'id' + analyticsId);
   }
   return analyticsId;
-}
+};
 
 const $recordAccess = async (userId) => {
-  const ref = db.collection('access').doc(userId)
+  const ref = db.collection('access').doc(userId);
   return ref.set({
     timestamp: new Date().getTime(),
   });
-}
+};
 
 const $recordConversion = (userId) => {
   const ref = db.collection('conversion').doc(userId);
   return ref.set({
     timestamp: new Date().getTime(),
   });
-}
+};
 
 const $refinementRecord = async () => {
-  const accessSnapshot = await db.collection('access')
-    .where("timestamp", ">", new Date(new Date() - (1000 * 60 * 15)).getTime()).get();
+  const accessSnapshot = await db
+    .collection('access')
+    .where('timestamp', '>', new Date(new Date() - 1000 * 60 * 15).getTime())
+    .get();
 
-  const conversionSnapshot = await db.collection('conversion')
-    .where("timestamp", ">", new Date(new Date() - (1000 * 60 * 60 * 24)).getTime()).get();
+  const conversionSnapshot = await db
+    .collection('conversion')
+    .where('timestamp', '>', new Date(new Date() - 1000 * 60 * 60 * 24).getTime())
+    .get();
 
   return {
     dayAccess: accessSnapshot.docs.length,
     dayConversion: conversionSnapshot.docs.length,
-  }
-}
+  };
+};
 
 const reflectRecord = (record) => {
   if (record.dayAccess > 0) {
@@ -184,11 +186,11 @@ const reflectRecord = (record) => {
   }
 
   const element = document.querySelectorAll('.toast');
-  for(let i = 0; i < element.length; i++) {
-    setTimeout(() => element[i].style.display = 'block', 2000 * (i + 1));
-    setTimeout(() => element[i].style.display = 'none', 2000 * (i + 1) + 10000);
+  for (let i = 0; i < element.length; i++) {
+    setTimeout(() => (element[i].style.display = 'block'), 2000 * (i + 1));
+    setTimeout(() => (element[i].style.display = 'none'), 2000 * (i + 1) + 10000);
   }
-}
+};
 
 /**
  * window.onloadで必要なイベントを実行
@@ -196,9 +198,9 @@ const reflectRecord = (record) => {
 window.onload = async () => {
   try {
     firebase.initializeApp({
-      apiKey: "AIzaSyDZX9RvCWbBOiRl0_heOshMEFUiI9QqD0g",
-      authDomain: "benaton-japan.firebaseapp.com",
-      projectId: "benaton-japan",
+      apiKey: 'AIzaSyDZX9RvCWbBOiRl0_heOshMEFUiI9QqD0g',
+      authDomain: 'benaton-japan.firebaseapp.com',
+      projectId: 'benaton-japan',
     });
     db = firebase.firestore();
     $recordAccess(getUserId());
@@ -211,7 +213,7 @@ window.onload = async () => {
   copyReserved();
   linkScroll();
   setTimeout(() => {
-    $refinementRecord().then(data => reflectRecord(data));
+    $refinementRecord().then((data) => reflectRecord(data));
   }, 5000);
 
   // ABテスト確認用
@@ -220,80 +222,99 @@ window.onload = async () => {
   // }
 };
 
-
 function getParam(name) {
   const url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
   const results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 /**
  * Windowがリサイズされた時、モバイル用メニューの設定をリセット
  */
-window.addEventListener("resize", (event) => {
-  const nav = document.querySelector('nav');
-  const link = document.querySelector('#mobile-menu-button');
-  link.classList.remove('active');
-  if (document.body.clientWidth > 800) {
-    nav.style.display = 'block';
-  } else {
-    nav.style.display = 'none';
-  }
-}, { passive: true });
+window.addEventListener(
+  'resize',
+  (event) => {
+    const nav = document.querySelector('nav');
+    const link = document.querySelector('#mobile-menu-button');
+    link.classList.remove('active');
+    if (document.body.clientWidth > 800) {
+      nav.style.display = 'block';
+    } else {
+      nav.style.display = 'none';
+    }
+  },
+  { passive: true },
+);
 
 /**
  * スクロール量に応じて、ヘッダーを縮小させるための
  * Classを付与・削除
  */
-document.addEventListener('scroll', (e) => {
-  const nav = document.querySelector('header');
-  let ticking = false;
-  if (!ticking) {
-    requestAnimationFrame(function() {
-      ticking = false;
-      const scrollAmount = document.documentElement.scrollTop;
+document.addEventListener(
+  'scroll',
+  (e) => {
+    const nav = document.querySelector('header');
+    let ticking = false;
+    if (!ticking) {
+      requestAnimationFrame(function () {
+        ticking = false;
+        const scrollAmount = document.documentElement.scrollTop;
 
-      if (scrollAmount > 60) {
-        nav.classList.add('minimum');
-      } else if (scrollAmount < 30) {
-        nav.classList.remove('minimum');
-      }
-    });
-    ticking = true;
-  }
-}, { passive: true });
+        if (scrollAmount > 60) {
+          nav.classList.add('minimum');
+        } else if (scrollAmount < 30) {
+          nav.classList.remove('minimum');
+        }
+      });
+      ticking = true;
+    }
+  },
+  { passive: true },
+);
 
 /**
  * aタグをクリックした時のイベントを
  * Google Analyticsに送信
  */
-document.addEventListener('click', (event) => {
-  try {
-    const path = event.path || (event.composedPath && event.composedPath());
-    const aTag = path.find(e => e.localName === 'a');
-    if (!aTag || !aTag.href) {
-      return;
-    }
-    const target = aTag.hash ? aTag.hash : aTag.href;
-    gtag('event', 'select_content', { 'content_type': target });
+document.addEventListener(
+  'click',
+  (event) => {
+    try {
+      const path = event.path || (event.composedPath && event.composedPath());
+      const aTag = path.find((e) => e.localName === 'a');
+      if (!aTag || !aTag.href) {
+        return;
+      }
+      const target = aTag.hash ? aTag.hash : aTag.href;
+      gtag('event', 'select_content', { content_type: target });
 
-    if (target.includes('reserve.resebook.jp') || target.includes('0798-37-2655')) {
-      $recordConversion(getUserId());
+      if (target.includes('reserve.resebook.jp') || target.includes('0798-37-2655')) {
+        $recordConversion(getUserId());
+      }
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) { console.log(e); }
-}, { passive: true });
+  },
+  { passive: true },
+);
 
-document.addEventListener('click', (event) => {
-  try {
-    const path = event.path || (event.composedPath && event.composedPath());
-    const toastTag = path.find(e => e.className === 'toast');
-    if (!toastTag) {
-      return;
+document.addEventListener(
+  'click',
+  (event) => {
+    try {
+      const path = event.path || (event.composedPath && event.composedPath());
+      const toastTag = path.find((e) => e.className === 'toast');
+      if (!toastTag) {
+        return;
+      }
+      toastTag.style.display = 'none';
+    } catch (e) {
+      console.log(e);
     }
-    toastTag.style.display = 'none';
-  } catch (e) { console.log(e); }
-}, { passive: true });
+  },
+  { passive: true },
+);
