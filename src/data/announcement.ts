eb06e71b3d206e6ab5637c5e71fe6a49f0e7b1ba
lebenaton <70837142+lebenaton@users.noreply.github.com>
2022-01-02 17:37:43 +0900

    Update announcement.ts

--- src/data/announcement.ts
+++ src/data/announcement.ts
@@ -2,7 +2,7 @@ import { IItem } from './item.interface';
 
 export const announcement: IItem[] = [
   {
-    image: 'assets/images/announcement/KIMG0563_1.JPG',
+    image: 'assets/images/announcement/KIMG0563_1.jpg',
     title: 'オードブルの盛り合わせはいかがですか？',
     body: 'ご家庭でベナトンのオードブルを！！<br>3～4名様で楽しんで頂けるサイズです。<br>税込み価格5000円～ご予算に合わせて内容が変わります、詳しくはお店までお問い合わせください。',
   },
