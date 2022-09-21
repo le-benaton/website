71797205d3fbd6c04df74381c06fbc7e878c3846
榊原昌彦 <sakakibara@rdlabo.jp>
2022-09-21 18:13:09 +0900

    chore

--- src/components/page-wines/page-wines.tsx
+++ src/components/page-wines/page-wines.tsx
@@ -22,7 +22,7 @@ export class PageWines {
   types: string[] = ['RED', 'WHITE', 'SPARKLING', 'CHAMPAGNE', 'ROSE', 'OTHER'];
 
   async componentDidRender() {
-    const wines = await fetch('https://przhp2ejw9.execute-api.ap-northeast-1.amazonaws.com/dev/group/2362-5951/0', {
+    const wines = await fetch('http://localhost:3000/group/2362-5951', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
