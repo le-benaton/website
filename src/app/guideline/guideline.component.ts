589e156c7358af32d9a9ca0fe6f4b7ff6a57a448
榊原昌彦 <sakakibara@rdlabo.jp>
2024-05-08 22:12:02 +0900

    chore

--- src/app/guideline/guideline.component.ts
+++ src/app/guideline/guideline.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { announcement } from '../../data/announcement';
 import { news } from '../../data/news';
 import { IonIcon } from '@ionic/angular/standalone';
@@ -9,6 +9,7 @@ import { IonIcon } from '@ionic/angular/standalone';
   imports: [IonIcon],
   templateUrl: './guideline.component.html',
   styleUrl: './guideline.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class GuidelineComponent {
   readonly announcement = announcement;
