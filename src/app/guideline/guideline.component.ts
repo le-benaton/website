cb0f2bb45e95540ead535b7c8f6ac1b464e4181d
榊原昌彦 <sakakibara@rdlabo.jp>
2024-05-08 13:58:01 +0900

    chore(): すべてを移行

---
+++ src/app/guideline/guideline.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { announcement } from '../../data/announcement';
+import { news } from '../../data/news';
+import {IonIcon} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-guideline',
+  standalone: true,
+  imports: [
+    IonIcon
+  ],
+  templateUrl: './guideline.component.html',
+  styleUrl: './guideline.component.scss'
+})
+export class GuidelineComponent {
+  readonly announcement = announcement;
+  readonly news = news;
+}
