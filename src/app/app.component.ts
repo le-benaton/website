01a356344477d368fab0a89d39475ae6819bfe4b
榊原昌彦 <sakakibara@rdlabo.jp>
2024-05-08 13:39:51 +0900

    feat(): Angularベースに変更

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'benaton';
+}
--- src/components/shared/router.ts
+++
@@ -1,3 +0,0 @@
-import { createRouter } from 'stencil-router-v2';
-
-export const Router = createRouter();
